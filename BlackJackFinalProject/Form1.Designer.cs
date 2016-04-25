8555e04f13cf504890e6dbfaf0a91708d7d836eb
mrdruff <mrdruff@outlook.com>
2016-04-25 13:31:56 -0400

    update form

--- BlackJackFinalProject/Form1.Designer.cs
+++ BlackJackFinalProject/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace BlackJackFinalProject
 {
-   partial class Form1
+   partial class BlackJack
    {
       /// <summary>
       /// Required designer variable.
@@ -28,9 +28,17 @@
       /// </summary>
       private void InitializeComponent()
       {
-         this.components = new System.ComponentModel.Container();
+         this.SuspendLayout();
+         // 
+         // BlackJack
+         // 
+         this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
          this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-         this.Text = "Form1";
+         this.ClientSize = new System.Drawing.Size(881, 504);
+         this.Name = "BlackJack";
+         this.Text = "Black Jack";
+         this.ResumeLayout(false);
+
       }
 
       #endregion
--- BlackJackFinalProject/Form1.cs
+++ BlackJackFinalProject/Form1.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace BlackJackFinalProject
 {
-   public partial class Form1 : Form
+   public partial class BlackJack : Form
    {
-      public Form1()
+      public BlackJack()
       {
          InitializeComponent();
       }
