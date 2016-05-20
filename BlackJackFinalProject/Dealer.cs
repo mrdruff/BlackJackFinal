e85a34cb6fca2476f93e0963b2f6f6fa2e2816f6
mrdruff <mrdruff@outlook.com>
2016-05-20 11:41:24 -0400

    betting and adding cards to hands

---
+++ BlackJackFinalProject/Dealer.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlackJackFinalProject
+{
+   class Dealer:Person
+   {
+      // constructor
+      public Dealer()
+      {
+
+      }
+
+      public override string Hit(Card selectedCard)
+      {
+         this.AddCard(selectedCard);
+         throw new NotImplementedException();
+      }
+
+      public override void Stand()
+      {
+         throw new NotImplementedException();
+      }
+   }
+}
